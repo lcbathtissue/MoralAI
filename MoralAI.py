a49bb06161778cd0c0de65ffa83fe3202c74c5b9
Alden O'Cain <alden.ocain@ontariotechu.net>
2023-03-28 18:36:32 -0400

    grid with headers

--- MoralAI.py
+++ MoralAI.py
@@ -7,7 +7,12 @@ def set_cell(x, y, value):
 
 def grid_to_str():
     result = ''
-
+    # header = "                      1                   2                   3                   4                   5                   6                   7                   8                   9                   10\n"
+    header = "   "
+    for i in range(1, 11):
+        header += f"{i:20}"
+    header += "\n"
+    result += header
     # add rows
     for i, row in enumerate(grid):
         row_str = ' '.join(row)
@@ -19,6 +24,7 @@ def grid_to_str():
             result += f'{row_num_str} {row_str} {row_num_str}\n'
         else:
             result += f'{row_num_str}  {row_str} {row_num_str}\n'
+    result += header
     return result
 
 
