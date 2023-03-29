c545391d694e3c5775e968377db8ff15fdcd517b
Alden O'Cain <alden.ocain@ontariotechu.net>
2023-03-28 23:12:10 -0400

    csv summary

--- MoralAI_Util.py
+++ MoralAI_Util.py
@@ -69,6 +69,35 @@ def save_csv_file(data, filename):
                 'k: Agent competitiveness: k=(f-h)/(g-h)': '',
             })
 
+def generate_summary_csv(input_file, output_file):
+    scenarios = set()
+    with open(input_file, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        rows = list(csv_reader)
+        for row in rows:
+            scenarios.add(row['Scenario number (1,2 or 3)'])
+
+    with open(output_file, mode='w', newline='') as summary_csv_file:
+        fieldnames = ['Scenario number (1,2 or 3)', 'Average happiness in each iteration', 'Average competitiveness in each iteration']
+        csv_writer = csv.DictWriter(summary_csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+        for scenario in scenarios:
+            happiness_sum = 0
+            competitiveness_sum = 0
+            iteration_count = 0
+            for row in rows:
+                if row['Scenario number (1,2 or 3)'] == scenario and row['Average happiness in each iteration']:
+                    happiness_sum += float(row['Average happiness in each iteration'])
+                    competitiveness_sum += float(row['k: Agent competitiveness: k=(f-h)/(g-h)'])
+                    iteration_count += 1
+            if iteration_count > 0:
+                avg_happiness = happiness_sum / iteration_count
+                avg_competitiveness = competitiveness_sum / iteration_count
+                csv_writer.writerow({'Scenario number (1,2 or 3)': scenario, 
+                                    'Average happiness in each iteration': avg_happiness, 
+                                    'Average competitiveness in each iteration': avg_competitiveness})
+
 def check_game_config_validity(GAME_CONFIG):
     if GAME_CONFIG['num_agents'] > GAME_CONFIG['MAX_num_agents']:
         print("Number of simulated agents is exceeding 'MAX_num_agents'\nEXITING..")
--- test.py
+++ test.py
@@ -1,17 +1,32 @@
-import os
+import csv
 
-def show_moves(grids_list):
-    grids_list = [
-        [[1, 2, 2], [3, 4, 2], [3, 4, 2]], 
-        [[5, 6, 2], [7, 8, 2], [3, 4, 2]], 
-        [[9, 10, 2], [11, 12, 2], [3, 4, 2]]
-    ]
+def generate_summary_csv(input_file, output_file):
+    scenarios = set()
+    with open(input_file, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        rows = list(csv_reader)
+        for row in rows:
+            scenarios.add(row['Scenario number (1,2 or 3)'])
 
-    for i, array in enumerate(grids_list):
-        if i > 0:
-            input("\nPress Enter to continue...")
-        os.system('cls' if os.name == 'nt' else 'clear')
-        for row in array:
-            print(" ".join(str(elem) for elem in row))
+    with open(output_file, mode='w', newline='') as summary_csv_file:
+        fieldnames = ['Scenario number (1,2 or 3)', 'Average happiness in each iteration', 'Average competitiveness in each iteration']
+        csv_writer = csv.DictWriter(summary_csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
 
-show_moves(None)
+        for scenario in scenarios:
+            happiness_sum = 0
+            competitiveness_sum = 0
+            iteration_count = 0
+            for row in rows:
+                if row['Scenario number (1,2 or 3)'] == scenario and row['Average happiness in each iteration']:
+                    happiness_sum += float(row['Average happiness in each iteration'])
+                    competitiveness_sum += float(row['k: Agent competitiveness: k=(f-h)/(g-h)'])
+                    iteration_count += 1
+            if iteration_count > 0:
+                avg_happiness = happiness_sum / iteration_count
+                avg_competitiveness = competitiveness_sum / iteration_count
+                csv_writer.writerow({'Scenario number (1,2 or 3)': scenario, 
+                                    'Average happiness in each iteration': avg_happiness, 
+                                    'Average competitiveness in each iteration': avg_competitiveness})
+
+generate_summary_csv("example_test_csv.csv", "test_csv_summary.csv")
